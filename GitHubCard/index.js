27a4a058bf7627a61936f24570087136df54be43
marcos <marcosmartinz176@gmail.com>
2022-02-10 19:09:35 -0500

    Finished all parts

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,30 @@
+const { default: axios } = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
+
+for (let i = 0; i < followersArray.length; i++) {
+  getGitCard(followersArray[i]);
+}
+
+function getGitCard(username) {
+  axios
+    .get(`https://api.github.com/users/${username}`)
+    .then((resp) => {
+      document.querySelector(".cards").appendChild(githubCard(resp.data));
+    })
+    .catch((err) => console.error(err));
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,26 +50,56 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+function githubCard(getInfo) {
+  const card = document.createElement("div");
+  const imgCard = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const cardName = document.createElement("h3");
+  const cardlogin = document.createElement("p");
+  const cardLocation = document.createElement("p");
+  const cardProfile = document.createElement("p");
+  const cardA = document.createElement("a");
+  const cardFollowers = document.createElement("p");
+  const cardFollowing = document.createElement("p");
+  const cardBio = document.createElement("p");
 
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  cardName.classList.add("name");
+  cardlogin.classList.add("username");
+
+  imgCard.src = getInfo.avatar_url;
+  imgCard.alt = "github user";
+  cardName.textContent = getInfo.name;
+  cardlogin.textContent = getInfo.login;
+  cardLocation.textContent = getInfo.location;
+  cardProfile.textContent = "Profile";
+  cardA.textContent = "Link to profile";
+  cardA.href = getInfo.html_url;
+  cardFollowers.textContent = `Followers: ${getInfo.followers}`;
+  cardFollowers.href = getInfo.followers_url;
+  cardFollowing.textContent = `Following: ${getInfo.following}`;
+  cardFollowing.href = getInfo.following_url;
+  cardBio.textContent = getInfo.bio;
+
+  card.appendChild(imgCard);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardlogin);
+  cardInfo.appendChild(cardLocation);
+  cardInfo.appendChild(cardProfile);
+  cardProfile.appendChild(cardA);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+
+  return card;
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
+    
 */
 
 /*
